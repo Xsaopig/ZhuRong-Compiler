1537d73936baab1a6111d3e0c04a76bced5001af
Xsaopig <996131974@qq.com>
2022-07-21 16:57:04 +0100

    全局变量已经完美生成

--- arm.s
+++ arm.s
@@ -4,17 +4,15 @@
 	.global d
 	.type d,%object
 	.data
-	.size d, 104
+	.size d, 48
 d:
-	.word 1065353216
-	.word 1073741824
-	.word 1077936128
-	.word 0
-	.word 1084227584
-	.word 0
-	.word 1088421888
-	.word 1090519040
-	.space 72
+	.word 1
+	.word 2
+	.word 3
+	.word 5
+	.word 7
+	.word 8
+	.space 24
 
 
 	.global e
@@ -22,7 +20,7 @@ d:
 	.data
 	.size e, 32
 e:
-	.word 0
+	.word 2
 	.word 1
 	.word 3
 	.word 4
